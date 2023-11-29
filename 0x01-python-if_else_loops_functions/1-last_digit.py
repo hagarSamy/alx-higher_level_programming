48f3f00f1c550f6a0e9db30ea65e7222f9374c51
HagarSamy <hagarsami63@gmai.com>
2023-11-29 13:37:39 +0200

    Checking last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 sign = 1 if number >= 0 else -1
 last_digit = sign * int(str(number)[-1])
 if last_digit == 0:
-   suffix = " and is 0"
+    suffix = " and is 0"
 elif last_digit > 5:
     suffix = " and is greater than 5"
 elif last_digit < 6 and last_digit != 0:

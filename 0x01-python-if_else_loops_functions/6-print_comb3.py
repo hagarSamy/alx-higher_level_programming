3912a400708b22930e65dd46b2228d0880805701
HagarSamy <hagarsami63@gmai.com>
2023-12-03 22:35:30 +0200

    Printig unique combinations in descending order

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 9):
-    for j in range (i+1, 10):
+for i in range(0, 9):
+    for j in range(i+1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
-        elif(i != j):
+        elif (i != j):
             print("{}{}".format(i, j), end=', ')

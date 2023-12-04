eaf2f4f2032fde3a2710cd2ccb2b346e1e2079f3
hagarSamy <hs1800@fayoum.edu.eg>
2023-12-04 05:46:02 +0200

    Importing more than a function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    addition = add(a,b)
+    addition = add(a, b)
     subtraction = sub(a, b)
     multiplication = mul(a, b)
     division = div(a, b)

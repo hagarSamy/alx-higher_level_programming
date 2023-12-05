f5e1b92b2a9c0562897223ff1ce3a4c4490449ec
hagarSamy <hs1800@fayoum.edu.eg>
2023-12-05 16:40:43 +0200

    arguments addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     res = 0
-    for i in range (1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         res += int(sys.argv[i])
     print("{}".format(res))

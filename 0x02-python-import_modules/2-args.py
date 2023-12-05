9be5d8b5df616bc90df0cedf7e9c7aacf47b34fc
HagarSamy <hagarsami63@gmai.com>
2023-12-05 15:57:08 +0200

    args in Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    args = "argument."
+else:
+    args = "arguments:"
+print("{} {}".format(len(sys.argv) - 1, args))
+for i in range (1, len(sys.argv)):
+    print("{}: {}".format(i, sys.argv[i]))

3fa155173d6b1f55ce002c8e5dd381cd977fe9d1
HagarSamy <hagarsami63@gmai.com>
2023-12-08 17:02:01 +0200

    Returning a new matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for row in matrix:
+        new.append([x * x for x in row])
+    return new
+    #return[[x * x for x in row] for row in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+Data structures in Python(sets and dictionaries)
+lambda, map, reduce and filter

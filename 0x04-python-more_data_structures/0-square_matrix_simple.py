f80736da68a358b375b826adb73668be5752ee38
HagarSamy <hagarsami63@gmai.com>
2023-12-08 17:02:50 +0200

    Returning a new matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new.append([x * x for x in row])
     return new
-    #return[[x * x for x in row] for row in matrix]
+    # return[[x * x for x in row] for row in matrix]

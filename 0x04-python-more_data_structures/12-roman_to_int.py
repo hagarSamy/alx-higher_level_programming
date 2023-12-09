3345b87609870d99b1b011cee77cc2c9416befcc
HagarSamy <hagarsami63@gmai.com>
2023-12-09 23:38:34 +0200

    Converting Roman numbers to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or isinstance(roman_string, str):
-        return 0
     romNums = {
         'I': 1,
         'V': 5,
@@ -11,11 +9,14 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    res = 0
-    for j in range(len(roman_string) - 1):
-        if romNums[roman_string[j]] >= romNums[roman_string[j+1]]:
-            res += romNums[roman_string[j]]
-        else:
-            res -= romNums[roman_string[j]]
-    res += romNums[roman_string[-1]]
-    return res
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    else:
+        res = 0
+        for j in range(len(roman_string) - 1):
+            if romNums[roman_string[j]] >= romNums[roman_string[j+1]]:
+                res += romNums[roman_string[j]]
+            else:
+                res -= romNums[roman_string[j]]
+        res += romNums[roman_string[-1]]
+        return res

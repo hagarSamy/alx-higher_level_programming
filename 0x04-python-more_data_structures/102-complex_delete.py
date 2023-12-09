58287c50388d414856ba502b92013d0e3eec7fb5
HagarSamy <hagarsami63@gmai.com>
2023-12-10 00:43:06 +0200

    Deleting keys according to values

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,5 @@
 def complex_delete(a_dictionary, value):
     keyDel = [k for k, v in a_dictionary.items() if v == value]    
     for k in keyDel:        
-            del a_dictionary[k]
+        del a_dictionary[k]
     return a_dictionary

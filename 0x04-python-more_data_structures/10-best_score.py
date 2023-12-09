4242371e8d3cd3597dd8d6a84b5d314f19b87b08
hagarSamy <hs1800@fayoum.edu.eg>
2023-12-09 22:17:53 +0200

    REturning key holding max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,7 @@
 def best_score(a_dictionary):
     return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
     # if not a_dictionary:
-        # return None
+    # return None
     # max_val = max(a_dictionary.values())
     # best_k = [k for k, v in a_dictionary.items() if v == max_val]
     # return best_k
-    

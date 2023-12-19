7ab985e091eea7623e0130af4d0cd29e60fb10bf
HagarSamy <hagarsami63@gmai.com>
2023-12-20 00:35:05 +0200

    Printing only if value is an integer and skipping others

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    c = 0
     for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
-                i += 1
-            except (IndexError, TypeError):
+                c += 1
+            except IndexError:
+                raise
+            except Exception:
                 pass
     print()
-    return i
+    return c

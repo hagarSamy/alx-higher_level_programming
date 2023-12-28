a0fec0be7bcaadf6a6a3a2f2483eb6cc6ae0cc10
HagarSamy <hagarsami63@gmai.com>
2023-12-28 20:42:34 +0200

    Creating a class square with a size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A class of a square with size'''
+
+
+class Square:
+    '''A class that creates a square with a certain size'''
+    def __init__(self, size):
+        self.__size = size

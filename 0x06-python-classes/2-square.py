bb460910ae487e163f827aba7e7de85330c9a58a
HagarSamy <hagarsami63@gmai.com>
2023-12-28 21:07:30 +0200

    Creating a class square with a size conditions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''A square defined by size'''
+
+
+class Square:
+    '''A class that defines defines a square by size with defaukt val.'''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size

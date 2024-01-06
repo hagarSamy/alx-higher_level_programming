749bd8f1dfe9019620ce12549c65e6243000f594
HagarSamy <hagarsami63@gmai.com>
2024-01-06 23:22:00 +0200

    calculating area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,18 +3,18 @@
 
 
 class Square:
-    '''A class that defines defines a square's area'''
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif (size < 0):
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+	'''A class that defines defines a square's area'''
+	def __init__(self, size=0):
+		if not isinstance(size, int):
+			raise TypeError("size must be an integer")
+		elif (size < 0):
+			raise ValueError("size must be >= 0")
+		else:
+			self.__size = size
 
-    @property
-    def size(self):
-        return self.__size
-    
-    def area(self):
-        return ((self.__size)**2)
+	@property
+	def size(self):
+		return self.__size
+
+	def area(self):
+		return ((self.__size)**2)

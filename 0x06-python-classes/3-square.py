5d5ca09f7b139013828ef9479d591ca69dfc2336
HagarSamy <hagarsami63@gmai.com>
2024-01-06 23:20:30 +0200

    calculating area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Returning square's area'''
+
+
+class Square:
+    '''A class that defines defines a square's area'''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    def area(self):
+        return ((self.__size)**2)

d4b86b027b04b80bf6f2312b2c3579cbbdcfef34
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-08 19:18:03 +0200

    Checks if an object belongs to a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,6 +12,4 @@ def is_same_class(obj, a_class):
     Return: True if true and False otherwise
     '''
 
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return (type(obj) is a_class)

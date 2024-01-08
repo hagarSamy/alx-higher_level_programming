d36faff20eebaee935e560bd653d58faa5d8abfc
HagarSamy <hagarsami63@gmai.com>
2024-01-08 21:14:13 +0200

    real inheritance ya man

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ that inherits from BaseGeometry (7-base_geometry.py).
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''A subclass that inherits from geometry
     Args:

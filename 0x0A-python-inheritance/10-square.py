85cc41efec17c5c61cea1a1cf453844ae9e3a57a
HagarSamy <hagarsami63@gmai.com>
2024-01-08 21:54:58 +0200

    a little more with str and class inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''a class Square that
+inherits from Rectangle (9-rectangle.py).
+'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''A subclass that inherits from rectangle
+    Args:
+            size (int): The size of the new Square.
+    '''
+    def __init__(self, size):
+        ''' initializes a new square
+        Args: size
+        '''
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''calculates area of a rectangle'''
+        return self.__size ** 2
+    def __str__(self):
+       '''Beautifies the output'''
+       ToReturn = "[" + type(self).__name__ + "]" + " "
+       ToReturn += str(self.__size) + "/"
+       ToReturn += str(self.__size)
+       return ToReturn

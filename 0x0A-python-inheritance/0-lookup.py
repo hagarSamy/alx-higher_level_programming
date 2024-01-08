44ab18f7f0484d0a6c36c4820b203e93f179e90f
HagarSamy <hagarsami63@gmai.com>
2024-01-08 18:48:12 +0200

    Getting back attributes in an object using dir function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,5 +2,8 @@
 '''A module that lists available attributes and methods of an object'''
 
 def lookup(obj):
-    '''returns all attributes and methods in an object'''
+    '''returns all attributes and methods in an object
+    Args: object
+    '''
+
     return dir(obj)

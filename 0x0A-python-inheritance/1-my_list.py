ee1213ffac4365c4a6ce6f91654154e9a28b4313
HagarSamy <hagarsami63@gmai.com>
2024-01-08 19:16:19 +0200

    Checks if an object belongs to a class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,9 @@ class MyList(list):
     '''
 
     def print_sorted(self):
+        """method takes self as arg
+        Args: self
+        Returns : void : print sorted list"""
         new = []
         for i in self:
             new.append(i)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''A module to returns True if the object is exactly an instance of the specified class
+otherwise False
+'''
+
+
+def is_same_class(obj, a_class):
+    '''checks if an object is an instance of a class
+    Args: an object and a class
+
+    Return: True if true and False otherwise
+    '''
+
+    if isinstance(obj, a_class):
+        return True
+    return False

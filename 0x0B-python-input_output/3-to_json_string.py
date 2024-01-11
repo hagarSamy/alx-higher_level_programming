1727cdeef88983c23772a5fdf19ed6fc4037c2b3
HagarSamy <hagarsami63@gmai.com>
2024-01-11 15:35:18 +0200

    From Pyhton to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''A module to write a
+function that returns the
+JSON representation of an
+object (string)
+'''
+import json
+
+def to_json_string(my_obj):
+    '''returns the json representation of an obj
+    Args: the object to convert
+    Return: the object's json representation
+    '''
+
+    NewObj = json.dumps(my_obj, sort_keys=True)
+    return NewObj

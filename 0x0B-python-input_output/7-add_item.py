4f111f6d3bf63348bc5eddc2f7fb95d47a69c002
HagarSamy <hagarsami63@gmai.com>
2024-01-11 19:15:40 +0200

    Adding command line arguments to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''A module to write a script
+that adds all arguments to a
+Python list, and then save
+them to a file
+'''
+import json
+import os
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys
+
+
+args = sys.argv[1:]
+
+if os.path.isfile("add_item.json"):
+    itemsToAdd = load_from_json_file("add_item.json")
+else:
+    itemsToAdd = []
+
+allToAdd = itemsToAdd + args
+
+save_to_json_file(allToAdd, "add_item.json")

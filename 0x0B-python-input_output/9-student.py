42fc1fed38188c492bda442b92fba34d29a921b9
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-11 22:23:55 +0200

    Retrieving a dict representation of a class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,4 +8,17 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        '''A function to initialize an instance()'''
+        '''A function to initialize an instance(instantiation)
+        Args: first and last names and age
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """A public method to retrieve a
+        dict representation of a st instance
+        """
+
+        return self.__dict__

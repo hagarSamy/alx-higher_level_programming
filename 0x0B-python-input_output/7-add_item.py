0a35750ece6b8a5252f75e78577954473afa316c
HagarSamy <hagarsami63@gmai.com>
2024-01-11 19:17:08 +0200

    Adding command line arguments to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,9 @@ them to a file
 '''
 import json
 import os
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-import sys
 
 
 args = sys.argv[1:]

e941ccb1e47d3878aba1baef6e5c21a44f224104
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-14 18:24:26 +0200

    Adding 2 ints with test edges

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' A module to write a function that
+adds 2 integers.
+and checks a test file
+'''
+
+
+def add_integer(a, b=98):
+    '''A function that adds two integers
+    Args: a and b set to default: 98
+    Return: sum of both integers
+    '''
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+       b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer or float")
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+Just a little for TDD
+Yes, Test Driven Development
+and adapting the test to fail, pass, refactored approach.

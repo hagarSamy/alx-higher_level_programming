306cd4b8b4a302b09fec9601e7c2dffdb3cd3ddf
HagarSamy <hagarsami63@gmai.com>
2024-01-14 18:33:25 +0200

    Adding 2 ints with test edges

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,7 +16,7 @@ def add_integer(a, b=98):
     if isinstance(b, float):
         b = int(b)
     if not isinstance(a, int):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or float")
     if not isinstance(b, int):
         raise TypeError("b must be an integer or float")
     return (a + b)

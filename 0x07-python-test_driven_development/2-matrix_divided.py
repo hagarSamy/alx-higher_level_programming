008da33fd58d556525ee478deab3ba538426bd9e
HagarSamy <hagarsami63@gmai.com>
2024-01-15 09:00:36 +0200

    Matrix divided with edge cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,20 +15,22 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/float")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/float")
     for row in matrix:
         if row == []:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/float")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/float")
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/float")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/float")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+                raise TypeError("matrix must be a matrix \
+                                (list of lists) of integers/floats")
     for i in range(len(matrix)):
         if len(matrix[i]) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    
     return [[round((element / div), 2) for element in row] for row in matrix]

c838c0d9fee8ee710cc4d8d1781275b641b3f6c6
HagarSamy <hagarsami63@gmai.com>
2024-01-17 00:19:26 +0200

    Updating attribute values using args no keyword arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -141,3 +141,17 @@ class Rectangle(Base):
             f"{self.__x}/{self.__y} - {self.__width}/{self.__height}"
         )
         return s
+
+    def update(self, *args):
+        '''assigns an argument to each attribute'''
+
+        if len(args) > 0:
+            self.__id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]

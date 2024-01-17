a98868330ef279d38cf3174a9663cb53ead17927
HagarSamy <hagarsami63@gmai.com>
2024-01-17 23:01:48 +0200

    Updating the size setter method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,108 +12,33 @@ class Square(Rectangle):
         '''Instatiation of a square class'''
 
         super().__init__(size, size, x, y, id)
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be > 0")
-        else:
-            self.__size = size
-
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = x
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
 
     @property
     def size(self):
         '''size getter'''
 
-        return self.__size
+        return self.width
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         '''size setter
         args: size
         '''
 
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size <= 0:
+        if value <= 0:
             raise ValueError("size must be > 0")
         else:
-            
-            self.width = size
-            self.height = size
-
-    @property
-    def x(self):
-        '''x getter'''
-
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-        '''x setter
-        args: x
-        '''
-
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = x
-
-    @property
-    def y(self):
-        '''y getter'''
-
-        return self.__y
-
-    @y.setter
-    def y(self, y):
-        '''y setter
-        args: y
-        '''
-
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
-
-    def area(self):
-        '''Calculates and returns the area of
-        a square
-        '''
-
-        return self.__size * self.__size
-
-    def display(self):
-        '''Prints hashes as square'''
-
-        for j in range(self.__y):
-            print()
-        for i in range(self.__size):
-            for k in range(self.__x):
-                print(" ", end="")
-            print("#" * self.__size)
+            self.width = value
+            self.height = value
 
     def __str__(self):
         '''beautifies the output'''
 
         s = (
             f"[{Square.__name__}] ({self.id}) "
-            f"{self.__x}/{self.__y} - {self.__size}"
+            f"{self.x}/{self.y} - {self.size}"
         )
         return s
 

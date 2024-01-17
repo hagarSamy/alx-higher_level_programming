f5d62b1bd93a303b7f3f704b2cd22443ad790fc9
HagarSamy <hagarsami63@gmai.com>
2024-01-18 01:24:59 +0200

    saving a class to a file as json str

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,21 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file'''
+
+        filename = cls.__name__ + ".json"
+        if list_objs is None or list_objs == []:
+            list_objs = []
+        else:
+            listDicts = []
+            for obj in list_objs:
+                listDicts.append(obj.to_dictionary())
+                '''same as
+                listDicts = [obj.to_dictionary() for obj in list_objs]
+                '''
+
+        with open(filename, "w") as jf:
+            jf.write(cls.to_json_string(listDicts))

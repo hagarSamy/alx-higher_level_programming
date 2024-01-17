ad3691f531b5aad7a810da8e128c68042bf9cf05
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-17 23:32:48 +0200

    a staic method that returns the json repr of a list of dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ''''A module to initiate a class'''
 
+import json
+
 
 class Base:
     '''A class to set self id'''
@@ -16,3 +18,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' returns the JSON string representation
+        of list_dictionaries
+        '''
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)

6ced2ce280f5876560cbf24626d6a11e71e10606
HagarSamy <hagarsami63@gmai.com>
2024-01-18 01:59:16 +0200

    creating a class with kwargs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,3 +56,14 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''returns an instance with all attributes already set
+        Args: dictionary: kword args
+        '''
+    
+        '''creating a dummy instance with arbitrary values'''
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy

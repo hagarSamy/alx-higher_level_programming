0c26cff5271f1dcbccaf23527b5d9ffba5b2ecca
HagarSamy <hagarsami63@gmai.com>
2024-02-22 01:40:22 +0200

    printing average with a chosen name using AS kw

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in my MySQL server.
+-- GROUP BY clause in SQL is used to group rows that have the same values in specified columns into summary rows
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;

d8004278b24a9768a7c71a8184aeaba5ed1233ef
HagarSamy <hagarsami63@gmai.com>
2024-02-22 00:30:16 +0200

    printing full description of a table in a certain db

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server.
-SHOW FULL COLUMNS FROM first_table
+-- This command is useful for viewing (SQL statement that was used to create the table) ==
+-- the structure of the table, including column definitions, indexes, and any table-level attributes such as
+-- storage engine, character set, and collation.
+SHOW CREATE first_table

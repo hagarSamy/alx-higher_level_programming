724652d74d252b1626bea5d5484eb2b728dd683b
HagarSamy <hagarsami63@gmai.com>
2024-02-24 13:39:04 +0200

    CREATING A USER AND a database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,8 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 -- user_0d_2 should have only SELECT privilege in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* FOR 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO `user_0d_2`@`localhost`
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`

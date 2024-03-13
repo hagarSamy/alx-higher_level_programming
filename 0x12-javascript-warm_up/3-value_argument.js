3860f2dfa5bf772f997969417d29fd3db4230015
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-13 23:14:14 +0200

    accessing differnt args in the argv array and utilizing while loop

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 // a value is undefined if the var is defined but given no vALUE
-if (typeof argv[2] === 'undefined') {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
 } else {
-    console.log(argv[2]);
+  let i = 2;
+  while (process.argv[i]) {
+    console.log(process.argv[i]);
+    i++;
+  }
 }

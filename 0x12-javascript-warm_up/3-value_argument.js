3d792b258515dba14c7b7778c4fda4946eec0f38
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-13 22:20:03 +0200

    accessing differnt args in the argv array

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-// a value is undefined if the var is there but given no vALUE
 if (typeof argv[2] === 'undefined') {
   console.log('No argument');
 } else {

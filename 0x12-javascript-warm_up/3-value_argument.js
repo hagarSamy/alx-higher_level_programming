cdc0dc0d428c7b0945cc959511738df1d2e7b5dd
HagarSamy <hagarsami63@gmai.com>
2024-03-14 22:57:59 +0200

    Accessing the arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// a value is undefined if the var is defined but given no vALUE
-if (typeof (process.argv[2]) === 'undefined') {
+const { argv } = require('node:process');
+if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  console.log(argv[2]);
 }

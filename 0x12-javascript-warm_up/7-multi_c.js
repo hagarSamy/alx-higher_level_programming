a4af9e0abd0ea0fc4ffbe7279673f2765af6ab8b
HagarSamy <hagarsami63@gmai.com>
2024-03-15 00:20:04 +0200

    looping with for

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,9 @@ const { argv } = require('process');
 const x = Number(argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   let i;
   for (i = 0; i < x; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 }

270bf2c74178a5f00fa13351731d075c5242f2e3
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-15 09:21:14 +0200

    using nested loops to print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const x = Number(argv[2]);
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  let h, w;
+  for (h = 0; h < x; h++) {
+    w = '';
+    for (let i = 0; i < x; i++) {
+      w += 'x';
+    }
+    console.log(w);
+  }
+}

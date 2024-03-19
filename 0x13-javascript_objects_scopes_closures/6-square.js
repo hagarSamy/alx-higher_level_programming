383923a82523c86ae337b8ee76be075f0735aa4b
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-19 17:14:01 +0200

    creating a class square that inherits from square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// using a different name for imported class
+// to avoid conflicts
+const PSquare = require('./5-square.js');
+class Square extends PSquare {
+  charPrint(c) {
+    if (c) {
+      let i, x, rec;
+      for (i = 0; i < this.height; i++) {
+      rec = '';
+        for (x = 0; x < this.width; x++) {
+          rec += c;
+        }
+        console.log(rec);
+      }
+    } else {
+        this.print();
+    }
+ }
+}
+module.exports = Square;

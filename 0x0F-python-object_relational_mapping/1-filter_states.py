720ec4fb1a1b1e2973c74a4bfe394b4227021c11
HagarSamy <hagarsami63@gmai.com>
2024-03-23 12:04:45 +0200

    MANIPULATING THE QUERY TO get a name with specefic starting letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
                          db=dbname)
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id ASC')
+    cur.execute('SELECT * FROM states WHERE \
+                name LIKE "N%" ORDER BY states.id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)

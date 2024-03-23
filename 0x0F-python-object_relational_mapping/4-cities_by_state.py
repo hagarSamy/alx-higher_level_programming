dfc11f1b11e05c4d61b7a65990916af17f7cf0cb
HagarSamy <hagarsami63@gmai.com>
2024-03-23 15:56:05 +0200

    listing cities and states from both tables joined

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 INNER JOIN states\
-                    WHERE states.id = cities.state_id\
+                    ON states.id = cities.state_id\
                         ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:

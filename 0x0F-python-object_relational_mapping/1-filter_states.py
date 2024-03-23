f6f4bb72dfa7dbdf3c1d85c9a70cfada6cdbcac3
HagarSamy <hagarsami63@gmai.com>
2024-03-23 12:12:10 +0200

    MANIPULATING THE QUERY TO get a name with specefic starting letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,8 +19,10 @@ if __name__ == "__main__":
                          db=dbname)
 
     cur = db.cursor()
+    ''' LIKE isn't case sensiteive, so i used
+    COLLATE that will handle case sensitivity '''
     cur.execute('SELECT * FROM states WHERE \
-                BINARY name LIKE "N%" ORDER BY states.id ASC')
+                name COLLATE utf8_bin LIKE "N%" ORDER BY states.id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)

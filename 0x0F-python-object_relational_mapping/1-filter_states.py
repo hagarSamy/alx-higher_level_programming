987a43658025fa2b2e05bacd3cd4d13f6b1bda5a
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-23 12:08:17 +0200

    MANIPULATING THE QUERY TO get a name with specefic starting letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute('SELECT * FROM states WHERE \
-                name LIKE "N%" ORDER BY states.id ASC')
+                BINARY name LIKE "N%" ORDER BY states.id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)

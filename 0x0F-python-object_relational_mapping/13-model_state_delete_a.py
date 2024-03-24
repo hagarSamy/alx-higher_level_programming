c6eff964c8b2b3869079969ea5b98432e3f7c0d7
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-24 21:54:36 +0200

    deleting objets using letter match with the name

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that deletes all State objects with a name containing the letter 'a'
+"""a script that deletes all State objects with
+a name containing the letter 'a'
 from the database hbtn_0e_6_usa
 """
 import sys

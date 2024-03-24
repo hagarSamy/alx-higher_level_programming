bbdb2b5628286c13cd03f33bdce6651f213512f4
HagarSamy <hagarsami63@gmai.com>
2024-03-24 17:35:33 +0200

    creating a class to inherit from Base as the table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,16 +2,9 @@
 ''' A module defining a State as an instance of Base'''
 from sqlalchemy import create_engine, Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
 
-db = sys.argv[1]
-usname = sys.argv[2]
-pswd = sys.argv[3]
 Base = declarative_base()
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(db, usname, pswd))
-
 
 class State(Base):
     ''' A class that inherits from Base'''
@@ -19,6 +12,3 @@ class State(Base):
     id = Column(Integer, autoincrement=True,
                 unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)

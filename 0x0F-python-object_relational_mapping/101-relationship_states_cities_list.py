4b080551d8bc2a2b92e7ea06e1fe6cef0e82a3eb
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-26 13:23:05 +0200

    more application on bidirectonal relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,10 +18,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    allSt = session.query(State).all()
+    allSt = session.query(State).order_by(State.id).all()
     for state in allSt:
         print(f'{state.id}: {state.name}')
         for city in state.cities:
             print(f'    {city.id}: {city.name}')
-    session.commit()
     session.close()

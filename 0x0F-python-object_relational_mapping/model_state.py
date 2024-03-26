49ffaec89b83cd10af0230e16be001f7777ae125
HagarSamy <hagarsami63@gmai.com>
2024-03-26 13:50:43 +0200

    little updates on models and relations

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' A module defining a State as an instance of Base'''
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    ''' A class that inherits from Base'''
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backpopulates='states')

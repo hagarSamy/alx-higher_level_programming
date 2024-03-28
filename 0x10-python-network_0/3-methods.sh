745b9aaed86c02eba8b105950fa7fcf13bd40541
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-28 21:17:55 +0200

    displaying allowed methods on a server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS "$1" | grep Allow | awk -F ": " '{print $2}'
+curl -sIX OPTIONS "$1" | grep Allow | awk -F ": " '{print $2}'

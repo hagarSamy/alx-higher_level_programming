652b7af4dd5fd90bfb1d3a6e217360b6986266b3
HagarSamy <hs1800@fayoum.edu.eg>
2024-04-29 22:06:29 +0300

    fetching a url using requests module and get method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+'''
+a Python script that fetches https://alx-intranet.hbtn.io/status
+'''
 
 
-import requests
-my_url = "https://alx-intranet.hbtn.io/status"
-with requests.get(my_url) as my_response:
-    if my_response.status_code == 200:
-        print(
-            "Body response:\n"
-            "\t- type: {}\n"
-            "\t- content: {}".format(
-                type(my_response.text),
-                my_response.text))
-# #!/usr/bin/python3
-# '''
-# a Python script that fetches https://alx-intranet.hbtn.io/status
-# '''
-
-
-# if __name__ == "__main__":
-#     import requests
-#     url = "https://alx-intranet.hbtn.io/status"
-#     response = requests.get(url)
-#     print("Body response:")
-#     res = response.text
-#     print("\t- type: {}".format(type(res)))
-#     print("\t- content: {}".format(res))
+if __name__ == "__main__":
+    import requests
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    res = response.text
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))

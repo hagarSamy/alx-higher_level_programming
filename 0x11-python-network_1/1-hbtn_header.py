4d5fd89401b6f260274689c84f6352b5b3f12b7f
HagarSamy <hs1800@fayoum.edu.eg>
2024-04-29 14:43:54 +0300

    extracting a certain value from the header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,8 @@ import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    id = response.getheader('X-Request-Id')
-print(id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        id = response.getheader('X-Request-Id')
+    print(id)

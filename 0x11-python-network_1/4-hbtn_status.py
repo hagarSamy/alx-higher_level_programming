1b524969f2d403225b737bd067c6fa0fbef9dd3b
HagarSamy <hs1800@fayoum.edu.eg>
2024-04-29 21:58:44 +0300

    fetching a url using requests module and get method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,28 @@
+# #!/usr/bin/python3
+# '''
+# a Python script that fetches https://alx-intranet.hbtn.io/status
+# '''
+
+
+# if __name__ == "__main__":
+#     import requests
+#     url = "https://alx-intranet.hbtn.io/status"
+#     response = requests.get(url)
+#     print("Body response:")
+#     res = response.text
+#     print("\t- type: {}".format(type(res)))
+#     print("\t- content: {}".format(res))
 #!/usr/bin/python3
-'''
-a Python script that fetches https://alx-intranet.hbtn.io/status
-'''
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 
 if __name__ == "__main__":
     import requests
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
+    my_url = "https://alx-intranet.hbtn.io/status"
+    req = requests.get(my_url)
     print("Body response:")
-    res = response.text
-    print("\t- type: {}".format(type(res)))
-    print("\t- content: {}".format(res))
+    content = req.text
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")

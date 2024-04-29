4f4209fdcc245efb0d9eadac2eb3f58391b31352
HagarSamy <hs1800@fayoum.edu.eg>
2024-04-29 17:22:34 +0300

    handling urllib.error.HTTPError exceptions

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+'''
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            response = response.read()
+            print ("{}".format(response.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))

6756b7b103949287ea82d4d11fe63324fd9a6078
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 01:07:50 +0300

    writing data to a file using fs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const FilePath = process.argv[2];
 const string = process.argv[3];
-const { error } = require('console');
+// const { error } = require('console');
 const fs = require('fs');
 
 fs.writeFile(FilePath, string, 'utf8', (err) => {

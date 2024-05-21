c0b0beaa1392c29dc01ef0e5bd9f90ab145444f8
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 01:33:00 +0300

    GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const url = process.argv[2];
-fs = require('fs');
-request = require('request');
+const request = require('request');
 
 request(url, (error, response, body) => {
   if (!error) {

a447b2c5dab52484030e870638bc3e1e262299d0
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 00:53:21 +0300

    reading data from a file using fs file system module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 if (process.argv[2]) {
-    const filename = process.argv[2];
-    const fs = require('fs');
-
-    try {
-        const data = fs.readFileSync(filename, 'utf8');
-        console.log(data);
-    } catch (error) {
+  const filename = process.argv[2];
+  const fs = require('fs');
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    console.log(data);
+  } catch (error) {
     console.error(error);
-    }
+  }
 }

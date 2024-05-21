f43c591963a86979f17a312dc25ad11695d6d078
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 01:45:38 +0300

    GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const request = require('request');
 
 request(url, (error, response, body) => {
   if (!error) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });

9eb07a9505b53a1e73cd85a2ca892aa3e4e81083
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 21:37:54 +0300

    number of movies by a character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
-const id = 18;
 const request = require('request');
 const movAPI = process.argv[2];
-const fullUrl = `${movAPI}/${id}`
+// const fullUrl = `${movAPI}/${id}`
 request(movAPI, (error, response, body) => {
   if (!error) {
-    const bodyObj = JSON.parse(body);
-    console.log(bodyObj);
+    let count = 0;
+    const films = JSON.parse(body).results;
+    films.forEach(film => {
+      if (film.characters.some(characterUrl => characterUrl.endsWith('/18'))) {
+        count++;
+      }
+    });
+    console.log(count);
   }
 });

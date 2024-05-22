5206191f5a939bf98bfc39003725d1e7831a51a5
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 22:44:05 +0300

    selected tasks from todos

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  const sum = {};
+  if (!error) {
+    const todos = JSON.parse(body);
+    todos.forEach(task => {
+      if (task.completed) {
+        if (!sum[task.userId]) {
+          sum[task.userId] = 0;
+        }
+        sum[task.userId]++;
+      }
+    });
+    console.log(sum);
+  }
+});

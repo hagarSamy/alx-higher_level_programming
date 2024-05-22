8f4e46eb1464ad3fe8638c678fce5b269bedac8a
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-22 22:06:56 +0300

    reading and writing both together

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,7 +7,6 @@ const file = process.argv[3];
 request(url, (error, response, body) => {
   if (!error) {
     const fs = require('fs');
-    const webpageData = fs.readFileSync(file, 'utf8');
-    fs.writeFileSync(file, webpageData, 'utf8');
+    fs.writeFileSync(file, body, 'utf8');
   }
 });

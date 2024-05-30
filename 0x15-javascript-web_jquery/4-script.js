48aa8eb1310168dd3ab18208013642372fd7f754
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-30 04:07:08 +0300

    toggling a class

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+// to toggle a class = adding it to an element if it doesn't already have it,
+// and removing it if it does.
+$('#toggle_header').click(function () {
+    $('header').toggleClass('red green');
+  });

5ce5c5aec15f8912cea53d384e63663151938a30
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-30 05:57:06 +0300

    ensuring DOM loading with js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// the event listener ensures that
+// the script runs when the HTML document has been fully loaded and parsed
+document.addEventListener('DOMContentLoaded', function() { 
+    document.querySelector('header').style.color = '#FF0000';
+ });
